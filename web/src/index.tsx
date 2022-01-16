f42b27cc07848e4b82b6af69971c2e1ef625e145
sshrik <chj878194@naver.com>
2022-01-16 20:01:48 +0900

    project(i18n): add i18next dependency

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle, { theme } from './GlobalStyle';
+import 'i18n';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
